8efb761ffb5e0a26ca15a0f82f855709c6cd6217
kriminskiy <93137873+kriminskiy@users.noreply.github.com>
2022-11-19 14:00:17 +0200

    add coomon and fonts in assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import './common/main.scss';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
